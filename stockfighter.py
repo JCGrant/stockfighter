ed95f15792aaf50b930f58e3aa08aab4b429ef32
JCGrant <jamescolin.grant@gmail.com>
2016-04-17 23:44:25 +0100

    Added functionality for multiple websocket clients to run at once.

--- stockfighter.py
+++ stockfighter.py
@@ -1,7 +1,10 @@
 import requests
 import os
 import json
-from ws4py.client.threadedclient import WebSocketClient
+from ws4py.client.threadedclient import WebSocketBaseClient
+from ws4py.manager import WebSocketManager
+import time
+
 
 class Stockfighter():
     base_url = 'https://api.stockfighter.io/ob/api'
@@ -15,6 +18,9 @@ class Stockfighter():
 
         self.session = requests.Session()
         self.session.headers.update({"X-Starfighter-Authorization": api_key})
+
+        self.websocket_clients = []
+        self.websocket_manager = WebSocketManager()
 
     def api_up(self):
         r = self.session.get(
@@ -81,32 +87,55 @@ class Stockfighter():
         r = self.session.get(url)
         return r.json()
 
+
+    class WebSocketClient(WebSocketBaseClient):
+        def __init__(self, url, callback, websocket_manager):
+            WebSocketBaseClient.__init__(self, url)
+            self.callback = callback
+            self.websocket_manager = websocket_manager
+
+        def handshake_ok(self):
+            print('Connected to Stockfighter web socket server.')
+            self.websocket_manager.add(self)
+
+        def close(self, code, reason=None):
+            print('Web socket closed.\nCode: {}\nReason: {}'.format(code, reason))
+
+        def received_message(self, message):
+            data = json.loads(message.data.decode('utf-8'))
+            self.callback(data)
+
     def ticker(self, callback, stock=None):
         url = 'wss://api.stockfighter.io/ob/api/ws/{}/venues/{}/tickertape'.format(
                 self.account, self.venue)
         if stock is not None:
             url += '/stocks/{}'.format(stock)
-        return self.WebSocketClient(url, callback)
+        self.websocket_clients.append(self.WebSocketClient(url, callback, self.websocket_manager))
 
     def executions(self, callback, stock=None):
         url = 'wss://api.stockfighter.io/ob/api/ws/{}/venues/{}/executions'.format(
                 self.account, self.venue)
         if stock is not None:
             url += '/stocks/{}'.format(stock)
-        return self.WebSocketClient(url, callback)
+        self.websocket_clients.append(self.WebSocketClient(url, callback, self.websocket_manager))
 
-    class WebSocketClient(WebSocketClient):
-        def __init__(self, url, callback):
-            WebSocketClient.__init__(self, url)
-            self.callback = callback
-            self.connect()
+    def start_websocket_clients(self):
+        try:
+            self.websocket_manager.start()
+            for client in self.websocket_clients:
+                client.connect()
 
-        def opened(self):
-            print('Connected to Stockfighter web socket server.')
+            while True:
+                for ws in self.websocket_manager.websockets.values():
+                    if not ws.terminated:
+                       break
+                else:
+                    break
+                time.sleep(3)
+        except KeyboardInterrupt:
+            self.shutdown_websocket_clients()
 
-        def closed(self, code, reason=None):
-            print('Web socket closed.\nCode: {}\nReason: {}'.format(code, reason))
-
-        def received_message(self, message):
-            data = json.loads(message.data.decode('utf-8'))
-            self.callback(data)
+    def shutdown_websocket_clients(self):
+        self.websocket_manager.close_all()
+        self.websocket_manager.stop()
+        self.websocket_manager.join()
